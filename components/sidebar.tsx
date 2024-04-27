3553da97f5902aaa3d9d3006a7300cb27629dd4f
Filipe <filipelehmannp@gmail.com>
2024-04-26 22:31:37 -0300

    feat: add sidebar basics

---
+++ components/sidebar.tsx
@@ -0,0 +1,10 @@
+export const Sidebar = () => {
+  return (
+    <div
+      className="flex bg-blue-500 h-full w-[256px] fixed lg:w-[256px] lg:fixed left-0
+     top-0 px-4 border-r-2 flex-col"
+    >
+      Sidebar
+    </div>
+  );
+};
