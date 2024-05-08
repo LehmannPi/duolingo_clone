6ef949303b48cbe4706db65aecbb99313abf5170
Filipe <filipelehmannp@gmail.com>
2024-05-07 22:32:41 -0300

    chore: improve main page responsivity

--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -1,3 +1,4 @@
+import { MobileHeader } from '@/components/mobile-header';
 import { Sidebar } from '@/components/sidebar';
 import React from 'react';
 
@@ -6,8 +7,9 @@ type Props = { children: React.ReactNode };
 const MainLayout = ({ children }: Props) => {
   return (
     <>
-      <Sidebar />
-      <main className="pl-[256px] h-full">
+      <MobileHeader />
+      <Sidebar className="hidden lg:flex" />
+      <main className="lg:pl-[256px] h-full pt-[50px] lg:pt-0">
         <div className="bg-red-500">{children}</div>
       </main>
     </>
