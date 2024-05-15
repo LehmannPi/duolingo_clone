49dd58448c5916af10bef5b8f94d32e82afc95be
Filipe <filipelehmannp@gmail.com>
2024-05-15 14:00:37 -0300

    feat: add feed and user progress sections

--- app/(main)/learn/page.tsx
+++ app/(main)/learn/page.tsx
@@ -1,7 +1,26 @@
+import FeedWrapper from '@/components/feed-wrapper';
+import SickyWrapper from '@/components/sticky-wrapper';
+import { UserProgress } from '@/components/user-progress';
+import Header from './header';
+
 type Props = {};
 
-const LearnPage = (props: Props) => {
-  return <div>Learn Page</div>;
+const LearnPage = () => {
+  return (
+    <div className="flex flex-row-reverse gap-[48px] px-6">
+      <SickyWrapper>
+        <UserProgress
+          activeCourses={{ title: 'German', imageSrc: 'de.svg' }}
+          hearts={5}
+          points={100}
+          hasActiveSubscription={false}
+        />
+      </SickyWrapper>
+      <FeedWrapper>
+        <Header title="German" />
+      </FeedWrapper>
+    </div>
+  );
 };
 
 export default LearnPage;
