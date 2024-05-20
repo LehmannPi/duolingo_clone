5422401fa216905d06da74365ccbf0eb95f87ea7
Filipe <filipelehmannp@gmail.com>
2024-05-19 23:32:19 -0300

    feat: add query to get user progress

--- db/queries.ts
+++ db/queries.ts
@@ -1,7 +1,27 @@
 import db from '@/db/drizzle';
+import { auth } from '@clerk/nextjs/server';
+import { eq } from 'drizzle-orm';
 import { cache } from 'react';
+import { userProgress } from './schema';
 
-// ? Makes it not care if it's called in many places throught the code. It calls the DB only once
+// ? cache makes it not care if it's called in many places throught the code. It calls the DB only once
+export const getUserProgress = cache(async () => {
+  const { userId } = await auth();
+
+  if (!userId) {
+    return null;
+  }
+
+  const data = await db.query.userProgress.findFirst({
+    where: eq(userProgress.userId, userId),
+    with: {
+      activeCourse: true,
+    },
+  });
+
+  return data;
+});
+
 export const getCourses = cache(async () => {
   const data = await db.query.courses.findMany();
   return data;
