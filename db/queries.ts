e454c125d736c64a2521eb6f0a0486562ec89343
Filipe <filipelehmannp@gmail.com>
2024-06-07 14:08:39 -0300

    feat: add getUnits with normalizeData

--- db/queries.ts
+++ db/queries.ts
@@ -2,7 +2,7 @@ import db from '@/db/drizzle';
 import { auth } from '@clerk/nextjs/server';
 import { eq } from 'drizzle-orm';
 import { cache } from 'react';
-import { courses, userProgress } from './schema';
+import { courses, units, userProgress } from './schema';
 
 // ? cache makes it not care if it's called in many places throught the code. It calls the DB only once
 export const getUserProgress = cache(async () => {
@@ -20,6 +20,48 @@ export const getUserProgress = cache(async () => {
   });
 
   return data;
+});
+
+export const getUnits = cache(async () => {
+  const userProgress = await getUserProgress();
+
+  if (!userProgress?.activeCourse) {
+    return [];
+  }
+
+  const data = await db.query.units.findMany({
+    where: eq(units.courseId, userProgress.activeCourseId),
+    with: {
+      lessons: {
+        with: {
+          challenges: {
+            with: {
+              challengeProgress: true,
+            },
+          },
+        },
+      },
+    },
+  });
+
+  // ! Heavy computation on the backend so the code can easily use the boolean completed
+  const normalizeData = data.map((unit) => {
+    const lessonsWithCompletedStatus = unit.lessons.map((lesson) => {
+      const allCompletedChallenges = lesson.challenges.every((challenge) => {
+        return (
+          challenge.challengeProgress &&
+          challenge.challengeProgress.length > 0 &&
+          challenge.challengeProgress.every((progress) => progress.completed)
+        );
+      });
+
+      return { ...lesson, completed: allCompletedChallenges };
+    });
+
+    return { ...unit, lessons: lessonsWithCompletedStatus };
+  });
+
+  return normalizeData;
 });
 
 export const getCourses = cache(async () => {
