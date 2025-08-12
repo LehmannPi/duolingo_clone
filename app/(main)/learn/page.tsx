562df846e0a8b73a962025a3c65a70b89a9bdb3f
Filipe <filipelehmannp@gmail.com>
2025-08-12 13:56:19 -0300

    feat: active lesson and percentage to learn page

--- app/(main)/learn/page.tsx
+++ app/(main)/learn/page.tsx
@@ -1,24 +1,37 @@
 import { redirect } from 'next/navigation';
 
+import { lessons, units as unitsSchema } from '@/db/schema';
 import FeedWrapper from '@/components/feed-wrapper';
 import SickyWrapper from '@/components/sticky-wrapper';
 import { UserProgress } from '@/components/user-progress';
-import { getUnits, getUserProgress } from '@/db/queries';
+
+import {
+  getCourseProgress,
+  getLessonPercentage,
+  getUnits,
+  getUserProgress,
+} from '@/db/queries';
 
 import Header from './header';
 import { Unit } from './unit';
 
 const LearnPage = async () => {
-  const unitsDataPromise = getUnits();
-  const userUserProgressPromise = getUserProgress();
+  const userProgressData = getUserProgress();
+  const courseProgressData = getCourseProgress();
+  const lessonPercentageData = getLessonPercentage();
+  const unitsData = getUnits();
 
-  const [units, userProgress] = await Promise.all([
-    unitsDataPromise,
-    userUserProgressPromise,
-  ]);
+  const [units, userProgress, courseProgress, lessonPercentage] =
+    await Promise.all([
+      unitsData,
+      userProgressData,
+      courseProgressData,
+      lessonPercentageData,
+    ]);
 
   // ! This clause enables us to not have to use optional chain in UserProgress props
-  if (!userProgress || !userProgress.activeCourse) return redirect('/courses');
+  if (!userProgress || !userProgress.activeCourse || !courseProgress)
+    return redirect('/courses');
 
   return (
     <div className="flex flex-row-reverse gap-[48px] px-6">
@@ -40,8 +53,14 @@ const LearnPage = async () => {
               title={unit.title}
               description={unit.description}
               lessons={unit.lessons}
-              activeLesson={undefined}
-              activeLessonPercentage={0}
+              activeLesson={
+                courseProgress.activeLesson as
+                  | (typeof lessons.$inferSelect & {
+                      unit: typeof unitsSchema.$inferSelect;
+                    })
+                  | undefined
+              }
+              activeLessonPercentage={lessonPercentage}
             />
             {/* {JSON.stringify(unit)} */}
           </div>
